d46f92bac99c46bc8de8a3402e68db236ae12dba
SonyaSot <96470319+SonyaSot@users.noreply.github.com>
2022-02-01 18:13:43 +0300

    Update grade.php

--- grade.php
+++ grade.php
@@ -1,10 +1,22 @@
-<?php
-$points = 0;
-foreach($_POST as $key=>$value)
-{
-$points = $points + $value;
-}
-echo $points;
-?>
-
-
+<!doctype html>
+<html lang="ru">
+  <head>
+    <meta charset="utf-8">
+    <title>Результаты</title>  
+    <link rel="stylesheet" href="style.css">
+  </head>
+    <body>
+        <div class="content">
+            <div class="content-category">Результаты прохождения теста:</div>
+        
+            <?php
+                $points = 0;
+                foreach($_POST as $key=>$value)
+                {
+                $points = $points + $value;
+                }
+                echo $points;
+                ?>
+        </div>
+    </body>
+</html>
